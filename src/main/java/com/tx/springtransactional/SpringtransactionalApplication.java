9500069396c2f8d3b53799b31e2a39237c51ff6e
leebuwon <bw1611@naver.com>
2024-02-27 19:15:51 +0900

    init commit

---
+++ src/main/java/com/tx/springtransactional/SpringtransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.tx.springtransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtransactionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tx/springtransactional/SpringtransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tx.springtransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
